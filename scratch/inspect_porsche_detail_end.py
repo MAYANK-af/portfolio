70c8e5e8ee4a8e1dd7c05d061c829295a25b1e3b
Mayank Yadav <my1220301@gmail.com>
2026-06-21 19:08:26 +0530

    clean: remove temporary scripts

--- scratch/inspect_porsche_detail_end.py
+++
@@ -1,6 +0,0 @@
-with open('static/js/main.e1c64cec.js', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-idx = content.find('porsche:{role:')
-if idx != -1:
-    print(content[idx+600:idx+1200])
